fc9ee2e762c7f1016ef662be915bdb098a04d8eb
Seema Ullal <seema.ullal@afterpay.com>
2021-03-21 01:08:55 -0700

    movie search w/ hard coded movies working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+
+const sampleMovieList = [
+  "The Matrix",
+  "The Avengers",
+  "Spider-Man: Homecoming",
+  "Onward",
+  "Fast & Furious Presents: Hobbs & Shaw",
+  "The Lovebirds",
+  "Wonder Woman",
+  "Gladiator",
+  "The Lord of the Rings: The Fellowship of the Ring",
+  "Aladdin",
+];
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    console.log("useEffect running");
+    setMovies(
+      sampleMovieList.filter((movie) =>
+        movie.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
+  }, [searchTerm]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Movie favorites</h2>
+      <p>Search for movies and select your favorites!</p>
+      <label htmlFor="movie-search" className="margin-right-10">
+        Search for a movie
+      </label>
+      <input
+        value={searchTerm}
+        onChange={(event) => setSearchTerm(event.currentTarget.value)}
+        id="movie-search"
+      />
+      {movies.map((movie) => {
+        return <div>{movie}</div>;
+      })}
     </div>
   );
 }
