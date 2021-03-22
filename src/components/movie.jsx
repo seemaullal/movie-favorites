2f115202fe241062adff5ed8e23431574e8b4444
Seema Ullal <seema.ullal@afterpay.com>
2021-03-21 19:22:07 -0700

    basic rendering/layout

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -1,8 +1,42 @@
 export default function Movie({ title, posterUrl }) {
   return (
-    <div>
-      <img alt="moviePoster" src={posterUrl} />
-      <p>{title}</p>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "1fr",
+        gridTemplateRows: "auto",
+        justifyContent: "center",
+      }}
+    >
+      <div className="grid-item">
+        <img
+          alt="moviePoster"
+          width={300}
+          height={455}
+          src={
+            posterUrl !== "N/A"
+              ? posterUrl
+              : "https://www.popcorn.app/assets/app/images/placeholder-movieimage.png"
+          }
+        />
+      </div>
+      <div className="grid-item">
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1 fr 1fr 1fr",
+            gridTemplateRows: "auto",
+          }}
+        >
+          <p style={{ justifySelf: "center" }}>{title}</p>
+          <button
+            onClick={() => console.log("favorited!")}
+            className="favorite-button"
+          >
+            ❤️
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
