5e1adc06249ede8445917265b58acb4f575a3cfa
Seema Ullal <seema.ullal@afterpay.com>
2021-05-14 10:33:44 -0700

    fix path

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 
-app = Flask(__name__, static_folder='../build', static_url_path='/')
+app = Flask(__name__, static_folder='./build', static_url_path='/')
 
 @app.route('/')
 def index():
