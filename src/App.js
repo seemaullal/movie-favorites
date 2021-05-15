e822bd4d742a65356014fecd7b7e60c36d5170ef
Seema Ullal <seema.ullal@afterpay.com>
2021-05-14 21:31:35 -0700

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import FavoriteMovies from "./components/FavoriteMovies";
 
 function App() {
-  return <FavoriteMovies />;
+  return (
+    <BrowserRouter>
+      <ul className="nav">
+        <li>
+          <Link to="/movies">Search for movies</Link>
+        </li>
+        <li>
+          <Link to="/favorites">See your favorites</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/movies">
+          <FavoriteMovies />
+        </Route>
+        <Route path="/favorites">
+          <h2>Your faves</h2>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
